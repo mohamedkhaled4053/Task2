09749fe73d2620ec60ad0212e55cff47af8b6490
Mohamed Khaled <mohamedkhaled4053@gmail.com>
2023-10-02 12:37:31 +0300

    search and sort employers

--- src/components/Employers/Employers.tsx
+++ src/components/Employers/Employers.tsx
@@ -1,26 +1,42 @@
 import { Input, Select, Tooltip } from "antd";
-import React from "react";
+import { useState } from "react";
 import { Icon } from "../Icon/Icon";
 import Employer from "../Employer/Employer";
 import { ProgramData } from "../../utils/types";
 import { stages } from "../../utils/mock";
-import './style.scss'
+import "./style.scss";
 
 type Props = {
   programData: ProgramData;
 };
 
 const Employers = ({ programData }: Props) => {
-  let employers = Object.values(programData.employers).sort(
-    (a, b) => a.employerTotal.now.applied - b.employerTotal.now.applied
-  );
+  let [sortBy, setSortBy] = useState<(typeof stages)[number]>();
+  let [isAscending, setIsAscending] = useState<boolean>(true);
+  let [search, setSearch] = useState<string>("");
+
+  let employers = Object.values(programData.employers)
+    .filter((employer) => employer.name.includes(search))
+    .sort((a, b) => {
+      if (!sortBy) return 0;
+      let first = a.employerTotal.now[sortBy];
+      let second = b.employerTotal.now[sortBy];
+      if (!first || !second) return 0;
+
+      if (isAscending) return first - second;
+      return second - first;
+    });
+
   return (
     <div className="employers">
       <h2 className="employers-header">Detailed Opportunity Overview</h2>
       <div className="filters">
         <Input
           className="search"
+          placeholder="Search by employer name"
           prefix={<Icon name="Search" />}
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
           suffix={
             <Tooltip title="Extra information">
               <Icon name="Circle_Warning" />
@@ -28,7 +44,18 @@ const Employers = ({ programData }: Props) => {
           }
         />
         <div className="filters-end">
-          <Select className="sort">
+          <Select
+            className="sort"
+            value={sortBy}
+            onChange={(val) => setSortBy(val)}
+            placeholder="sort by"
+            suffixIcon={
+              <Icon
+                onClick={() => setIsAscending(!isAscending)}
+                name={isAscending ? "ascending" : "descending"}
+              />
+            }
+          >
             {stages.map((status) => (
               <Select.Option value={status}>{status}</Select.Option>
             ))}
